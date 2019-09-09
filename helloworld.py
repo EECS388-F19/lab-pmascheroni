1a0113383282d821041801b269e837036e248b36
Peter Mascheroni <petermascheroni@0587406544.wireless.umich.net>
2019-09-09 15:44:17 -0400

    first commit of hello world

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+from random import *
+
+if __name__ == "__main__":
+    a = randint(1, 100)
+    b = randint(1, 100)
+    c = a + b
+    d = c/2.0
+    print('Peter')
+    print(a)
+    print(b)
+    print('Sum = ' + str(c))
+    print('Average = ' + str(b))
